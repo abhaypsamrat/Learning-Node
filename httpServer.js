d5112fa2779cd309aeaca15b6328fa7ddd42732c
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-06-26 19:25:21 +0530

    added htttp module

--- httpServer.js
+++ httpServer.js
@@ -1,8 +1,14 @@
-const http = require("http");
+const http = require('http');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  console.log(req.url)
   res.statusCode = 200;
-  res.setHeader("Content-type", "text/html");
-});
+  res.setHeader('Content-type', 'text/html')
+  res.end('<h1>This is Abhay Pratap</h1><p>hey this the way to learn NodeJs</p>');
+})
+
+server.listen(port, ()=>{
+  console.log(`Server is listing on port ${port}`);
+})
