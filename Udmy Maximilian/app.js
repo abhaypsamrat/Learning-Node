f70e43de963b0c6e297ebc4439d8d6b6104ff73b
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-08-22 13:08:18 +0530

    Handling Different Routes

--- Udmy Maximilian/app.js
+++ Udmy Maximilian/app.js
@@ -1,16 +1,19 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 
-app.use((req, res, next)=>{
-    console.log("This is middleware");
+app.use('/', (req, res, next)=>{
+    console.log("This always runs");
     next();
 })
-app.use((req, res, next)=>{
+
+app.use('/add-product',(req, res, next)=>{
+    console.log("This is middleware");
+    res.send('<h1>the "Add Product" Page</h1>')
+})
+
+app.use('/',(req, res, next)=>{
     console.log("Another middleware");
     res.send("<h1>This is html text</h1>")
 })
 
-const server  = http.createServer(app);
-
-server.listen(3000)
+app.listen(3000);
